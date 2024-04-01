1ab3b4838b0ef5309fe27c81ae85153a181a1e3d
CoreyMcCoy <coreyamccoy@gmail.com>
2024-04-01 14:06:21 -0500

    added google login, save user info to the database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
+import { connectMongoDB } from '@/lib/mongodb';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import User from '@/models/User';
 
 const authOptions = {
   providers: [
@@ -8,6 +10,39 @@ const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === 'google') {
+        const { name, email } = user;
+        try {
+          await connectMongoDB();
+          const userExists = await User.findOne({ email });
+
+          if (!userExists) {
+            const res = await fetch('http://localhost:3000/api/user', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name,
+                email,
+              }),
+            });
+
+            if (res.ok) {
+              return user;
+            }
+          }
+        } catch (error) {
+          console.error('Failed to create user', error);
+        }
+      }
+
+      return user;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
